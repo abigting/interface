b42f6b6095484e5c57663eb3ee64eab51789864d
Fiona <1153531620@qq.com>
2020-03-29 16:56:32 +0800

    feat(接口)-用户注册 登录 修改密码

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-const { app, pool, Result } =require('./connect')
-const login = require('./router/login')
+/**
+ * @description 入口文件
+ * @author Feng.xiuting
+ * @date 2020/3/29
+ */
+const { app, pool } =require('./connect')
+const user = require('./router/user')
 app.all('*', (req, res, next) => {
   //这里处理全局拦截，一定要写在最上面
   next()
+})
+app.get('/', (req,res) => {  //首页路由
+  res.sendFile(__dirname+'/'+'index.html')
 })
 app.all('/', (req, res) => {
   pool.getConnection((err, conn) => {
@@ -10,7 +18,7 @@ app.all('/', (req, res) => {
     pool.releaseConnection(conn) // 释放连接池，等待别的连接使用
   })
 })
-app.use('/login', login)
+app.use('/user', user)
 app.listen(8088, () => {
-  console.log('服务启动')
+  console.log('服务启动','localhost:8088')
 })
